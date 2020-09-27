05ec5887d5d0ff2dd61651be836a5aa7ce9069ab
Artback <artback@protonmail.com>
2020-09-27 20:55:48 +0200

    change module url

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,7 @@ package main
 
 import (
 	"github.com/micro/go-micro"
-	proto "github.com/artback/kafka_microservices_go/greeter"
+	proto "github.com/artback//greeter"
 	"golang.org/x/net/context"
 	"log"
 )
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module https://github.com/artback/golang-microservices
+module github.com/artback/golang-microservices
 
 go 1.15
 
